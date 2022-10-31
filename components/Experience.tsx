64a7f768be382abc9ea30ae85d5a459d95ec03cd
zahra <benmedzahra20@gmail.com>
2022-10-31 15:23:41 +0100

    change experience

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -10,19 +10,23 @@ const Experience = () => {
       <div className='grid md:grid-cols-3 mt-10 '>
         
 <a href="#" className="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-    <p className="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">wimtech | Rabat | 2022/07-2022/08</h5>
+    <p className="font-normal text-gray-700 dark:text-gray-400">
+     </p>
 </a>
 
 <a href="#" className="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-    <p className="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">NonAcademy | Bangladesh | 2022/05-2022/06</h5>
+    <p className="font-normal text-gray-700 dark:text-gray-400">
+      
+    </p>
 </a>
 
 <a href="#" className="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-    <p className="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">ENSEM | casa | 2021/06-2021/07</h5>
+    <p className="font-normal text-gray-700 dark:text-gray-400"></p>
 </a>
+
 
       
         
