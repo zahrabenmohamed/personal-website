52137adae7098f959ee6e42e224446ffc1e179b6
zahra <benmedzahra20@gmail.com>
2022-11-02 10:56:27 +0100

    change

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -18,13 +18,14 @@ const Experience = () => {
 <a href="#" className="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
     <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">NonAcademy | Bangladesh | 2022/05-2022/06</h5>
     <p className="font-normal text-gray-700 dark:text-gray-400">
+
       
     </p>
 </a>
 
 <a href="#" className="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
     <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">ENSEM | casa | 2021/06-2021/07</h5>
-    <p className="font-normal text-gray-700 dark:text-gray-400"></p>
+    
 </a>
 
 
