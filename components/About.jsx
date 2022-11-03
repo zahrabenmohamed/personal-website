544bf9e142044f7219f3ccd2213227cae730f48e
zahra <benmedzahra20@gmail.com>
2022-11-03 15:45:25 +0100

    changes -v2

--- components/About.jsx
+++ components/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-import AboutImg from '../public/assets/ZAHRA-benmohamed.jpg';
+import AboutImg from '../public/assets/projects/about.jpg';
 import {HiDownload} from 'react-icons/hi';
 import { motion } from 'framer-motion';
 
